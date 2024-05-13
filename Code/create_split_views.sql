0afe2979fd55677306f00eb78757860ed0bd7acc
Ankitha S <ankithasures@umass.edu>
2024-05-13 11:06:20 -0400

    Removed unneccesary files

--- Code/create_split_views.sql
+++
@@ -1,5 +0,0 @@
-create view married as
-select * from census where marital_status in (' Married-AF-spouse', ' Married-civ-spouse', ' Married-spouse-absent',' Separated');
-
-create view unmarried as
-select * from census where marital_status in (' Never-married', ' Widowed',' Divorced');
